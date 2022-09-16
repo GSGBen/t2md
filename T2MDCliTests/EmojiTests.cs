f3d9eb2d5e0111d2740b836dadba2e1c9f89f41a
Ben Renninson <ben@goldensyrupgames.com>
2022-09-16 21:27:41 +1000

    Create replacement function (untested)

---
+++ T2MDCliTests/EmojiTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldenSyrupGames.T2MD.Tests
+{
+    [TestClass]
+    public class EmojiTests
+    {
+        [TestMethod]
+        public void ReplaceEmoji_StringWithEmoji_ReplacesEmoji()
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
