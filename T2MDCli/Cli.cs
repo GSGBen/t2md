2bd0d83d717e266617af02c516905b5c16993461
Ben Renninson <ben@goldensyrupgames.com>
2022-09-16 19:45:44 +1000

    Add the URL of the card at the top to allow searching references (#23)

--- T2MDCli/Cli.cs
+++ T2MDCli/Cli.cs
@@ -652,8 +652,17 @@ namespace GoldenSyrupGames.T2MD
             CliOptions options
         )
         {
-            // inject the full title into its output
-            var descriptionContents = $"# {trelloCard.Name}\n\n{trelloCard.Desc}";
+            // inject the full title, and short url, into its output. Add a prefix to the short URL
+            // to make the original more easily greppable: a search of just the URL will show all
+            // files that reference it.
+            var descriptionContents =
+                $"# {trelloCard.Name}\n"
+                + $"\n"
+                + $"Original URL: {trelloCard.ShortUrl}\n"
+                + $"\n"
+                + $"---\n"
+                + $"\n"
+                + $"{trelloCard.Desc}";
             // sort the cards in order unless specified otherwise
             var descriptionFilename = options.NoNumbering
                 ? $"{usableCardName}.md"
